c02e93ee798cb6c13ebe8e894a8033942d3a6f82
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-01-17 21:11:47 +0900

    Game over scene

---
+++ GWJ/Src/UI/GameOver/GameOver.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+var tween_values = [-1200, 0]
+
+func _ready():
+	_start_tween()
+
+func _start_tween():
+	$Tween.interpolate_property($BackGround/GameOverMan, "position:y", tween_values[0], tween_values[1], 1.5, $Tween.TRANS_SINE, $Tween.EASE_IN_OUT)   
+	$Tween.start()
+
+func _on_tween_completed(object, key):
+	$AnimationPlayer.play("fadeIn")
+
+

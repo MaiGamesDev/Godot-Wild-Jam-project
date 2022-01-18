2d2b89025e36aca1ef90e30ed30cd98c93bd54b1
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-01-18 18:41:50 +0900

    pause menu update

--- GWJ/Src/UI/PauseMenu/PauseMenu.gd
+++ GWJ/Src/UI/PauseMenu/PauseMenu.gd
@@ -8,10 +8,15 @@ func _input(event):
 	if event is InputEventKey:
 		if Input.is_action_just_pressed("open_PauseMenu"):
 			visible = !visible
+			get_tree().paused = visible
 
 func _on_Resume_pressed():
 	hide()
+	get_tree().paused = false
+	
 func _on_Setting_pressed():
 	$Menu/SettingPopUp.show()
+	
 func _on_MainMenu_pressed():
+	get_tree().paused = false
 	get_tree().change_scene("res://Src/UI/MainMenu/MainMenu.tscn")

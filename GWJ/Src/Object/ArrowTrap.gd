096855072e5d4698370086d8d9e04e14139b8f21
MaiGamesDev <94523847+MaiGamesDev@users.noreply.github.com>
2022-01-23 20:27:44 +0900

    level 1 and level 2 finished

--- GWJ/Src/Object/ArrowTrap.gd
+++ GWJ/Src/Object/ArrowTrap.gd
@@ -17,3 +17,11 @@ func _on_body_entered(_body: Node) -> void:
 	var arrow = Arrow.instance()
 	add_child(arrow)
 	arrow.start(fire_direction)
+	play_shoot_sound()
+
+func play_shoot_sound():
+	var sfx = load("res://Src/Sound/SFX_Player.tscn").instance()
+	var sound = load("res://Asset/Sound/SFX/Arrow_Shoot_And_Hit.wav")
+	sfx.volume_db = 0
+	sfx.stream = sound
+	add_child(sfx)
